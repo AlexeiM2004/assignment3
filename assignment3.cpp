bcc163747be6915902b5ce7b18b33128a47d1e4a
AlexeiM2004 <alexei.maiorov@student.manchester.ac.uk>
2026-03-05 16:27:10 +0000

    Assignment 3 Repository ready for use

---
+++ assignment3.cpp
@@ -0,0 +1,14 @@
+// 
+// Loads a file containing course module information, filters sorts and displays information based on users requests and calculates statistics based on the exam result.
+// Alexei Maiorov, Student ID: [11022726]
+// Date: [07/02/2026]
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+int main()
+{
+    return 0;
+}
