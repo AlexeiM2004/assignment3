0a8f34c4a1548a51d2c3b8d0a1a118dee6fa9b0f
AlexeiM2004 <alexei.maiorov@student.manchester.ac.uk>
2026-03-06 11:27:14 +0000

    The main.cpp will now read off data from two text files, detectors.txt and sources.txt. These text files will contain comma seperated data of the source and detector information.

--- main.cpp
+++ main.cpp
@@ -9,11 +9,73 @@
 #include <cmath>
 #include "source.h"
 #include "detector.h"
+#include <sstream>
+#include <fstream>
 
 
 int main()
 {
-    //Read csv and create the sources & detectors.
-    std::cout << "Test";
+    //Read in the sources file
+
+    std::ifstream source_file("sources.txt");
+    std::string source_data;
+
+    std::vector<Source> sources;
+
+    while(std::getline(source_file,source_data))
+    {
+        std::stringstream ss(source_data);
+        std::string type,date,activity_string,ID_string;
+        if(std::getline(ss, type, ',') && std::getline(ss, date, ',') && std::getline(ss, activity_string, ',') && std::getline(ss, ID_string, ','))
+        {
+            try
+            {
+                // Convert strings into double and int
+                double activity = std::stod(activity_string);
+                int ID = std::stoi(ID_string);
+  
+                //This converts the string from the file into a double
+                Source s(type, date, activity, ID);
+                sources.push_back(s);
+                
+            } catch(...) {}
+        }
+    }
+    source_file.close();
+
+    //Read in the detectors file
+
+    std::ifstream detector_file("detectors.txt");
+    std::string detector_data;
+
+    std::vector<Detector> detectors;
+
+    while(std::getline(detector_file,detector_data))
+    {
+        std::stringstream ss(detector_data);
+        std::string type,status_string;
+        if(std::getline(ss, type, ',') && std::getline(ss, status_string, ','))
+        {
+            try
+            {
+                // Converting status string into a bool
+                bool status;
+                if (status_string == "ON")
+                {
+                    status = true;
+                }else{
+                    status = false; 
+                }
+
+                Detector d(type, status);
+                detectors.push_back(d);
+                
+            } catch(...) {}
+        }
+    }
+    detector_file.close();
+
+    std::cout << "Loaded " << sources.size() << " sources successfully.\n";
+    std::cout << "Loaded " << detectors.size() << " detectors successfully.\n";
     return 0;
 }
